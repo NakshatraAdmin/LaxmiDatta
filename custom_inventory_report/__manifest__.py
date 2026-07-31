52049e73c14eb0cc13f69790e411441e8afd2ba4
Chirag Dudhait <chirag.dudhait143@gmail.com>
2026-07-31 16:32:50 +0530

    [add] Custom inventory report

---
+++ custom_inventory_report/__manifest__.py
@@ -0,0 +1,12 @@
+{
+    'name': 'Custom Inventory Report',
+    'version': '17.0.1.0.0',
+    'category': 'Inventory',
+    'summary': 'Laxmidatta inventory PDF layout',
+    'depends': ['stock'],
+    'data': [
+        'report/report_deliveryslip.xml',
+    ],
+    'installable': True,
+    'license': 'LGPL-3',
+}
